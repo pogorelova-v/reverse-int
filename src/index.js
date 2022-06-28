f342ea9a54b80f25ae997c55881687369ded9719
pogorelova-v <gorelikovavn@gmail.com>
2022-06-28 10:31:01 +0300

    feat: cancel

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function reverse (n) {
-  
+    
+    let newN = ''; 
+
+    if (n == 0){
+        newN = 0;
+    } else if ( n > 0 ){
+        for (let i = String(n).length - 1; i >= 0; i--) { 
+            newN += String(n)[i]; 
+        } 
+    } else {
+        n = n * -1;
+        for (let i = String(n).length - 1; i >= 0; i--) { 
+            newN += String(n)[i]; 
+        } 
+    }
+        
+        return newN; 
+
 }
